1517b36021c0e1984fbaec2bff7dcf389aab1f50
akiyoshi83 <mail@akiyoshi.info>
2015-01-28 19:45:37 +0900

    generate task scafold
    $ bin/rails g scaffold task name:string
    $ bin/rake db:migrate
    $ bin/rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |i|
+  Task.create({
+    name: "task_#{i}"
+  })
+end
